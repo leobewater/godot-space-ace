26d1fac4df10daf7ac81b6f204a08ba5627fb497
leobewater <mmbee888@gmail.com>
2023-12-28 08:36:15 -0500

    set up enemy basic with timer, hitbox and visible screen notifier

--- boom/boom.gd
+++ boom/boom.gd
@@ -8,10 +8,6 @@ func _ready():
 	SoundManager.play_explosion_random(sound)
 
 
-func _process(delta):
-	pass
-
-
 # remove self after finished playing animation
 func _on_animation_finished():
 	queue_free()
---
+++ enemies/enemy_basic.gd
@@ -0,0 +1,27 @@
+extends PathFollow2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_laser_timer_timeout():
+	pass # Replace with function body.
+
+
+func _on_visible_on_screen_notifier_2d_screen_entered():
+	pass # Replace with function body.
+
+
+func _on_visible_on_screen_notifier_2d_screen_exited():
+	pass # Replace with function body.
+
+
+func _on_hit_box_area_entered(area):
+	pass # Replace with function body.
