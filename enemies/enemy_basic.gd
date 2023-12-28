fb4a912798aecac1ba77f6fb187dd70d818aa765
leobewater <mmbee888@gmail.com>
2023-12-28 08:52:22 -0500

    turn enemy basic rotation off and fix play animation bug

--- enemies/enemy_basic.gd
+++ enemies/enemy_basic.gd
@@ -13,18 +13,19 @@ var _player_ref: Player
 var _speed: float
 var _can_shoot: bool = false
 var _dead: bool = false
+var _anim_string: String
 
 
 func _ready():
 	_player_ref = get_tree().get_first_node_in_group(GameData.GROUP_PLAYER)
 	if !_player_ref: 
 		queue_free()
-	animated_sprite_2d.play()
+	animated_sprite_2d.play(_anim_string)
 
 
 func setup(speed: float, anim_name: String) -> void:
 	_speed = speed
-	animated_sprite_2d.animation = anim_name
+	_anim_string = anim_name
 	
 
 func _process(delta):
