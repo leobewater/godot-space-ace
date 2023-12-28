27c07129d252896564f0debd6a6f929bfdf654cd
leobewater <mmbee888@gmail.com>
2023-12-28 08:07:19 -0500

    use object maker to create explosion

--- level/level.gd
+++ level/level.gd
@@ -8,4 +8,6 @@ func _ready():
 func _process(delta):
 	if Input.is_key_pressed(KEY_Q) == true:
 		GameManager.load_main_scene()
-
+		
+	if Input.is_key_pressed(KEY_E) == true:
+		ObjectMaker.create_explosion(Vector2(100, 200))
--- singletons/object_maker.gd
+++ singletons/object_maker.gd
@@ -1,11 +1,27 @@
 extends Node
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+enum SCENE_KEY { EXPLOSION }
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+const SIMPLE_SCENES = {
+	SCENE_KEY.EXPLOSION: preload("res://explosion/explosion.tscn")
+}
+	
+
+func add_child_deferred(child_to_add) -> void:
+	get_tree().root.add_child(child_to_add)
+	
+
+func call_add_child(child_to_add) -> void:
+	call_deferred("add_child_deferred", child_to_add)
+
+
+func create_simple_scene(start_pos: Vector2, key: SCENE_KEY) -> void:
+	var new_exp = SIMPLE_SCENES[key].instantiate()
+	new_exp.global_position = start_pos
+	call_add_child(new_exp)
+
+
+func create_explosion(start_pos: Vector2) -> void:
+	create_simple_scene(start_pos, SCENE_KEY.EXPLOSION)
