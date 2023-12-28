432e4c0dc74859a36a075b007b2cfc7c227c8f28
leobewater <mmbee888@gmail.com>
2023-12-28 15:04:03 -0500

    show multiple booms when enemy dies

--- enemies/enemy_basic.gd
+++ enemies/enemy_basic.gd
@@ -69,7 +69,23 @@ func shoot() -> void:
 	get_tree().root.add_child(b)
 	start_shoot_timer()
 	
+
+# creats booms in multiple locations for each enemy
+func make_booms() -> void:
+	for b in booms.get_children():
+		ObjectMaker.create_boom(b.global_position)
 	
+	
+func die() -> void:
+	if _dead == true:
+		return
+	_dead= true
+	
+	set_process(false)
+	make_booms()
+	queue_free()
+
+
 func _on_laser_timer_timeout():
 	shoot()
 
@@ -90,5 +106,4 @@ func _on_hit_box_area_entered(area):
 
 
 func _on_health_bar_died():
-	set_process(false)
-	queue_free()
+	die()
